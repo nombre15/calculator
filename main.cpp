d9b62268a31bac710d4dc6853ca600b7cd8f08f3
nombre15 <81451283+nombre15@users.noreply.github.com>
2021-03-26 20:25:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,47 +14,48 @@ int main(){
     float sum;
     int x1 = x;
     int y1 = y1;
-ecuacion:
-   cout << "que quieres hacer?" << endl;
-   cout << "1=suma, 2=resta, 3=multiplicacion, 4=division, 5=elevar, 6=numero al azar, 7=suma de raices, 8=porcentaje, 9=raiz cuadrada, 10=logaritmo" << endl;
+equation:
+   cout << "choose one" << endl;
+   cout << "1=addition, 2=substract, 3=multiply, 4=divide, 5=power, 6=random number, 7=square root sum, 8=percentage, 9=square root, 10=logarithm" << endl;
    cin >> thing;
 
    if(thing == 1){
-    cout << "\ningresa los numeros a sumar" << endl;
+    cout << "\nenter the numbers" << endl;
     cin >> x >> y;
     sum = x + y;
-    cout << "el resultado es: " << sum;
+    cout << "the result is: " << sum;
    }
 
    if(thing == 2){
-    cout << "\ningresa los numeros a restar" << endl;
+    cout << "\nenter the numbers" << endl;
     cin >> x >> y;
     sum = x - y;
-    cout << "el resultado es: " << sum;
+    cout << "the result is: " << sum;
    }
 
    if(thing == 3){
     cout << "\ningresa los numeros a multiplicar" << endl;
     cin >> x >> y;
     sum = x * y;
-    cout << "el resultado es: " << sum;
+    cout << "the result is: " << sum;
    }
 
    if(thing == 4){
-    cout << "\ningresa los numeros a dividir" << endl;
+    cout << "\nenter the numbers" << endl;
     cin >> x >> y;
     sum = x / y;
-    cout << "el resultado es: " << sum;
+    cout << "the result is: " << sum;
    }
 
    if(thing == 5){
-    cout << "\ningresa los numeros a elevar" << endl;
+    cout << "\nenter the numbers" << endl;
     cin >> x >> y;
     sum = pow(x, y);
+    cout << "the result is: " << sum;
    }
 
    if (thing == 6){
-    cout << "\ningresa dos numeros" << endl;
+    cout << "\nenter two numbers" << endl;
     cin >> x1 >> y1;
     srand(time(0));
         for(int x = x; x < 1; x++){
@@ -63,36 +64,36 @@ ecuacion:
    }
 
    if(thing == 7){
-    cout << "\ningresa los numeros a sumar" << endl;
+    cout << "\nenter the numbers" << endl;
     cin >> x >> y;
     sum = sqrt(x) + sqrt(y);
-    cout << "el resultado es " << sum << endl;
+    cout << "the result is: " << sum << endl;
    }
 
    if(thing == 8){
-    cout << "\ningresa el numero numero total y luego el numero para el porcentaje" << endl;
+    cout << "\nfirst enter the total number and then the percentage number" << endl;
     cin >> x >> y;
     sum = (y*100)/x;
-    cout << "\n" << y << " es el " << sum << "% de " << x << endl;
+    cout << "\n" << y << " is " << sum << "% of " << x << endl;
    }
 
    if(thing == 9){
-    cout << "\ningresa el numero para raiz cuadrada" << endl;
+    cout << "\nenter the number" << endl;
     cin >> x;
     sum = sqrt(x);
-    cout << "el resultado es " << sum << endl;
+    cout << "the result is: " << sum << endl;
    }
 
    if(thing == 10){
-    cout << "\ningresa el numero para logaritmo" << endl;
+    cout << "\nenter the number" << endl;
     cin >> x;
     sum = log(x);
-    cout << "el resultado es " << sum << endl;
+    cout << "the result is: " << sum << endl;
    }
 
    else{
-    cout << "\nesa no es una opcion\n" << endl;
-    goto ecuacion;
+    cout << "\nthat is not an option\n" << endl;
+    goto equation;
    }
 
    return 0;
